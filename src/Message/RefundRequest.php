110016c9795c19c7c1a8fd9cdf14736103670305
saalid <amirrezasalari1997@gmail.com>
2025-04-27 14:50:29 +0330

    feat(refund): add refund service to omnipay package

--- src/Message/RefundRequest.php
+++ src/Message/RefundRequest.php
@@ -6,6 +6,8 @@
 
 namespace Omnipay\Stripe\Message;
 
+use Omnipay\Common\Exception\InvalidRequestException;
+
 /**
  * Stripe Refund Request.
  *
@@ -51,69 +53,50 @@ namespace Omnipay\Stripe\Message;
  */
 class RefundRequest extends AbstractRequest
 {
-    /**
-     * @return bool Whether the application fee should be refunded
-     */
+    public function getEndpoint(): string
+    {
+        return $this->endpoint . '/refunds';
+    }
+
+    public function getHttpMethod(): string
+    {
+        return 'POST';
+    }
+
     public function getRefundApplicationFee()
     {
         return $this->getParameter('refundApplicationFee');
     }
 
-    /**
-     * Whether to refund the application fee associated with a charge.
-     *
-     * From the {@link https://stripe.com/docs/api#create_refund Stripe docs}:
-     * Boolean indicating whether the application fee should be refunded
-     * when refunding this charge. If a full charge refund is given, the
-     * full application fee will be refunded. Else, the application fee
-     * will be refunded with an amount proportional to the amount of the
-     * charge refunded. An application fee can only be refunded by the
-     * application that created the charge.
-     *
-     * @param bool $value Whether the application fee should be refunded
-     *
-     * @return AbstractRequest
-     */
-    public function setRefundApplicationFee($value)
+    public function setRefundApplicationFee($value): RefundRequest
     {
         return $this->setParameter('refundApplicationFee', $value);
     }
 
-    /**
-     * @return bool Whether the transfer should be reversed
-     */
     public function getReverseTransfer()
     {
         return $this->getParameter('reverseTransfer');
     }
 
-    /**
-     * Whether to refund the application fee associated with a charge.
-     *
-     * From the {@link https://stripe.com/docs/connect/destination-charges#issuing-refunds Stripe docs}:
-     * Charges created on the platform account can be refunded using the
-     * platform account's secret key. When refunding a charge that has a
-     * `destination[account]`, by default the destination account keeps the
-     * funds that were transferred to it, leaving the platform account to
-     * cover the negative balance from the refund. To pull back the funds
-     * from the connected account to cover the refund, set the
-     * `reverse_transfer` parameter to true when creating the refund
-     *
-     * @param bool $value Whether the transfer should be refunded
-     *
-     * @return \Omnipay\Common\Message\AbstractRequest
-     */
-    public function setReverseTransfer($value)
+    public function setReverseTransfer($value): RefundRequest
     {
         return $this->setParameter('reverseTransfer', $value);
     }
 
-    public function getData()
+    /**
+     * @throws InvalidRequestException
+     */
+    public function getData(): array
     {
-        $this->validate('transactionReference', 'amount');
+        $this->validate('transactionReference');
 
-        $data = array();
-        $data['amount'] = $this->getAmountInteger();
+        $data = [];
+
+        $data['charge'] = $this->getTransactionReference();
+
+        if ($this->getAmountInteger()) {
+            $data['amount'] = $this->getAmountInteger();
+        }
 
         if ($this->getRefundApplicationFee()) {
             $data['refund_application_fee'] = 'true';
@@ -125,9 +108,8 @@ class RefundRequest extends AbstractRequest
 
         return $data;
     }
-
-    public function getEndpoint()
+    protected function createResponse($data, $headers = []): RefundResponse
     {
-        return $this->endpoint.'/charges/'.$this->getTransactionReference().'/refund';
+        return $this->response = new RefundResponse($this, $data, $headers);
     }
 }
